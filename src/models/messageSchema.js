ff7a4ee62b45e4ced1493e4c21de3f6dc1689cee
Ronal <ronal.sanx21@gmail.com>
2025-06-06 16:00:15 -0500

    Agregar campo ara destinatario mensaje

--- src/models/messageSchema.js
+++ src/models/messageSchema.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 const MessageSchema = new mongoose.Schema(
   {
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
     content: { type: String, required: true },
-    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
   },
   { timestamps: true }
 );
