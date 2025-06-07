ebe73a164ed5750e6c7adf96dced6a3af0d84c93
Ronal <ronal.sanx21@gmail.com>
2025-06-07 18:49:18 -0500

    Documentacion

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,50 +1,49 @@
 import { Server, Socket } from "socket.io"
 import Message from "./messageSchema.js";
 
+/**
+ * Clase Chat para gestionar la lógica de los eventos de chat usando Socket.IO.
+ */
 class Chat {
 
     /**
-   * @param {Server} io - Instancia de Socket.IO Server
-   */
+     * Crea una instancia de Chat.
+     * @param {Server} io - Instancia de Socket.IO Server.
+     */
     constructor(io) {
         /** @type {Server} */
         this.io = io;
 
-
+        // Inicializa los eventos del chat
         this.chatEvents();
     }
 
-
+    /**
+     * Define y gestiona los eventos de chat para las conexiones de Socket.IO.
+     */
     chatEvents() {
-
+        // Se ejecuta cuando un cliente se conecta al servidor de sockets
         this.io.on('connection', (
-             /** @type {import("socket.io").Socket} */ socket
+            /** @type {import("socket.io").Socket} */ socket
         ) => {
 
+            // Escucha el evento 'enviar-mensajes' enviado por el cliente
             socket.on('enviar-mensajes', async (data) => {
-
                 const { sender, receiver, content } = data
 
                 try {
+                    // Crea y guarda un nuevo mensaje en la base de datos
                     const message = new Message({ sender, receiver, content })
                     await message.save()
+                    const mensajeObject = message.toObject()
+                    socket.emit('enviar-mensaje', mensajeObject )
                 } catch (error) {
-                    
+                    // Maneja errores al guardar el mensaje
                     throw `Error al guardar el mensaje ${error}`
                 }
-            
-            
-                
             })
-
- 
-
-            
-
         })
     }
 }
-
-
 
 export default Chat
